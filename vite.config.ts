cdc7c5860129aa50eba4a5e6c550ba62e7f6fb23
liaozhimingandy@qq.com <liaozhimingandy@qq.com>
2024-10-31 10:56:55 +0800

    fix(core): 修复缺少内容问题 1.去除了混淆功能插件

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
 import AutoImport from 'unplugin-auto-import/vite';
 import path from 'path';
 import removeConsole from 'vite-plugin-remove-console';
-import vitePluginBundleObfuscator from "vite-plugin-bundle-obfuscator";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,43 +30,6 @@ export default defineConfig({
             // 默认在生产环境移除console
             exclude: ['error', 'warn'], // 可选：保留 error 和 warn 信息
         }),
-       vitePluginBundleObfuscator({
-        excludes: [],
-        enable: true,
-        log: true,
-        autoExcludeNodeModules: false,
-        threadPool: false,
-        options: {
-          compact: true,
-          controlFlowFlattening: true,
-          controlFlowFlatteningThreshold: 1,
-          deadCodeInjection: false,
-          debugProtection: false,
-          debugProtectionInterval: 0,
-          disableConsoleOutput: false,
-          identifierNamesGenerator: "hexadecimal",
-          log: false,
-          numbersToExpressions: false,
-          renameGlobals: false,
-          selfDefending: true,
-          simplify: true,
-          splitStrings: false,
-          stringArray: false,
-          stringArrayCallsTransform: false,
-          stringArrayCallsTransformThreshold: 0.5,
-          stringArrayEncoding: [],
-          stringArrayIndexShift: true,
-          stringArrayRotate: true,
-          stringArrayShuffle: true,
-          stringArrayWrappersCount: 1,
-          stringArrayWrappersChainedCalls: true,
-          stringArrayWrappersParametersMaxCount: 2,
-          stringArrayWrappersType: "variable",
-          stringArrayThreshold: 0.75,
-          unicodeEscapeSequence: false,
-        },
-      }),
-
     ],
     resolve: {
         alias: {
